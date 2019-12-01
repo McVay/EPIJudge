7c9e1943438d2c3c914fafe5ec793108999c449e
Dylan McVay <dfmcvay@gmail.com>
2019-12-01 11:16:19 -0500

    Solved the two sum problem

--- epi_judge_python/two_sum.py
+++ epi_judge_python/two_sum.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
 
-
 def has_two_sum(A, t):
-    # TODO - you fill in here.
-    return True
+
+
+    left, right = 0, len(A) - 1
+    while left <= right:
+        currentSum = A[left] + A[right]
+        if currentSum == t:
+            return True
+        elif currentSum > t:
+            right -= 1
+        else:
+            left += 1
+
+    return False
 
 
 if __name__ == '__main__':
